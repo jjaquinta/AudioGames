257bae55bb67961a951b0b6d162b4bad54fbd527
Jo Jaquinta <jo_grant@us.ibm.com>
2020-11-15 23:10:45 -0500

    Name on baseline recognized. Fixes #1

--- jo.six.swords/src/jo/audio/companions/app/logic/DefaultLogic.java
+++ jo.six.swords/src/jo/audio/companions/app/logic/DefaultLogic.java
@@ -1,9 +1,17 @@
 package jo.audio.companions.app.logic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.function.BiConsumer;
+
 import org.json.simple.JSONObject;
 
 import jo.audio.companions.app.BaseStateHandler;
 import jo.audio.companions.app.CombatStateHandler;
+import jo.audio.companions.data.CompCompanionBean;
 import jo.audio.companions.data.CompEncounterBean;
 import jo.audio.companions.data.CompOperationBean;
 import jo.audio.companions.data.CompRoomBean;
@@ -118,133 +126,128 @@ public class DefaultLogic
         }
         return false;
     }
+    
+    private static List<FuzzyBaseMatcher> mBaseSingletons = new ArrayList<>();
+    private static void addBaseSingleton(String names, BiConsumer<CompState, BaseStateHandler> action)
+    {
+        for (StringTokenizer st = new StringTokenizer(names, ","); st.hasMoreTokens(); )
+            mBaseSingletons.add(new FuzzyBaseMatcher(st.nextToken(), action));
+    }
+    static
+    {
+        addBaseSingleton("look,examine,read sign,read post", (state,h) -> h.doLook(state));
+        addBaseSingleton("read deed", (state,h) -> h.doEquip(state, null, null));
+        addBaseSingleton("north", (state,h) -> h.doNorth(state));
+        addBaseSingleton("south,stuff,self", (state,h) -> h.doSouth(state));
+        addBaseSingleton("east,beast", (state,h) -> h.doEast(state));
+        addBaseSingleton("west", (state,h) -> h.doWest(state));
+        addBaseSingleton("characters", (state,h) -> h.doWho(state));
+        addBaseSingleton("time", (state,h) -> { state.respond(WhoLogic.getAboutTime(state));
+            state.respond(WhoLogic.getAboutMoon(state));});
+        addBaseSingleton("sun", (state,h) -> state.respond(WhoLogic.getAboutTime(state)));
+        addBaseSingleton("moon", (state,h) -> state.respond(WhoLogic.getAboutMoon(state)));
+        addBaseSingleton("straight,forward,continue", (state,h) -> {
+                switch (state.getContext().getUser().getLastMoveDirection())
+                {
+                    case CompOperationBean.NORTH:
+                        h.doMove(state, "north");
+                        break;
+                    case CompOperationBean.SOUTH:
+                        h.doMove(state, "south");
+                        break;
+                    case CompOperationBean.EAST:
+                        h.doMove(state, "east");
+                        break;
+                    case CompOperationBean.WEST:
+                        h.doMove(state, "west");
+                        break;
+                }
+            });
+        addBaseSingleton("repeat,replay", (state,h) -> h.handleRepeat(state));
+        addBaseSingleton("help", (state,h) -> h.doHelp(state));
+        addBaseSingleton("follow,track", (state,h) -> {
+                CompUserBean user = state.getUser();
+                CompEncounterBean encounter = user.getEncounter();
+                DebugUtils.trace("Attemtping to follow, spoorid="+encounter.getLastSpoorID()+", int="+user.getInteractions()+", spInt="+encounter.getLastSpoorInteraction());
+                if (!StringUtils.isTrivial(encounter.getLastSpoorID()) && (user.getInteractions() <= encounter.getLastSpoorInteraction() + 1))
+                    h.doAbout(state, "wilhemina");
+                else
+                    throw new IllegalStateException();
+            });
+        addBaseSingleton("turn off", (state,h) -> h.doStop(state));
+        addBaseSingleton("bounty,bounties", (state,h) -> {
+                if ("castleOffice".equals(state.getContext().getLocation().getRoomID()))
+                {
+                    CompRoomBean room = state.getContext().getRoom();
+                    JSONObject params = room.getParams();
+                    LookLogic.doMoreBounties(state, params);
+                }
+                else
+                    throw new IllegalStateException();
+            });
+    }
 
+    @SuppressWarnings("unchecked")
+    private static <T,U> boolean evalFuzzies(List<? extends FuzzyStateMatcher<U>> matchers, String raw, U h, CompState state)
+    {
+        for (Object m : matchers)
+        {
+            FuzzyStateMatcher<U> mm = (FuzzyStateMatcher<U>)m;
+            if (mm.eval(raw))
+                if (mm.exec(state, h))
+                    return true;
+        }
+        Collections.sort(matchers, new Comparator<FuzzyStateMatcher<U>>() {
+            @Override
+            public int compare(FuzzyStateMatcher<U> o1, FuzzyStateMatcher<U> o2)
+            {
+                return o2.count - o1.count;
+            }
+        });
+        for (FuzzyStateMatcher<U> m : matchers)
+            if (m.count < 100)
+                if (m.exec(state, h))
+                    return true;
+        return false;
+
+    }
+    
     public static boolean doSingleInvocations(CompState state, BaseStateHandler h,
             String raw)
     {
         DebugUtils.trace("Attemtping to default, raw="+raw);
-        if (raw.indexOf("look") >= 0)
-        {
-            h.doLook(state);
-            return true;
-        }
-        if (raw.indexOf("examine") >= 0)
-        {
-            h.doLook(state);
-            return true;
-        }
-        if (raw.indexOf("read") >= 0)
-        {
-            if ((raw.indexOf("sign") >= 0) || (raw.indexOf("post") >= 0))
-            {
-                h.doLook(state);
-                return true;
-            }
-            else if ((raw.indexOf("deed") >= 0))
-            {
-                h.doEquip(state, null, null);
-                return true;
-            }
-        }
-        if (raw.indexOf("north") >= 0)
-        {
-            h.doNorth(state);
-            return true;
-        }
-        if ((raw.indexOf("south") >= 0) || (raw.indexOf("stuff") >= 0) || (raw.indexOf("self") >= 0))
-        {
-            h.doSouth(state);
-            return true;
-        }
-        if ((raw.indexOf("east") >= 0) || (raw.indexOf("beast") >= 0))
-        {
-            h.doEast(state);
-            return true;
-        }
-        if (raw.indexOf("west") >= 0)
-        {
-            h.doWest(state);
-            return true;
-        }
-        if (raw.indexOf("characters") >= 0)
-        {
-            h.doWho(state);
-            return true;
-        }
-        if (raw.indexOf("time") >= 0)
-        {
-            state.respond(WhoLogic.getAboutTime(state));
-            state.respond(WhoLogic.getAboutMoon(state));
-            return true;
-        }
-        if (raw.indexOf("sun") >= 0)
-        {
-            state.respond(WhoLogic.getAboutTime(state));
-            return true;
-        }
-        if (raw.indexOf("moon") >= 0)
-        {
-            state.respond(WhoLogic.getAboutMoon(state));
-            return true;
-        }
-        if ((raw.indexOf("straight") >= 0) || (raw.indexOf("forward") >= 0) || (raw.indexOf("continue") >= 0))
-        {
-            switch (state.getContext().getUser().getLastMoveDirection())
-            {
-                case CompOperationBean.NORTH:
-                    h.doMove(state, "north");
-                    return true;
-                case CompOperationBean.SOUTH:
-                    h.doMove(state, "south");
-                    return true;
-                case CompOperationBean.EAST:
-                    h.doMove(state, "east");
-                    return true;
-                case CompOperationBean.WEST:
-                    h.doMove(state, "west");
-                    return true;
-            }
-        }
-        if ((raw.indexOf("repeat") >= 0) || (raw.indexOf("replay") >= 0))
-        {
-            h.handleRepeat(state);
-            return true;
-        }
-        if ((raw.indexOf("follow") >= 0) || (raw.indexOf("track") >= 0))
-        {
-            CompUserBean user = state.getUser();
-            CompEncounterBean encounter = user.getEncounter();
-            DebugUtils.trace("Attemtping to follow, spoorid="+encounter.getLastSpoorID()+", int="+user.getInteractions()+", spInt="+encounter.getLastSpoorInteraction());
-            if (!StringUtils.isTrivial(encounter.getLastSpoorID()) && (user.getInteractions() <= encounter.getLastSpoorInteraction() + 1))
-            {
-                h.doAbout(state, "wilhemina");
-                return true;
-            }
-        }
-        if (raw.indexOf("help") >= 0)
-        {
-            h.doHelp(state);
-            return true;
-        }
-        if (raw.indexOf("turn off") >= 0)
-        {
-            h.doStop(state);
-            return true;
-        }
-        if ((raw.indexOf("bounty") >= 0) || (raw.indexOf("bounties") >= 0))
-        {
-            if ("castleOffice".equals(state.getContext().getLocation().getRoomID()))
-            {
-                CompRoomBean room = state.getContext().getRoom();
-                JSONObject params = room.getParams();
-                LookLogic.doMoreBounties(state, params);
-                return true;
-            }
-            return false;
-        }
-        return false;
+        List<FuzzyBaseMatcher> matchers = new ArrayList<>();
+        addContextual(matchers, state);
+        matchers.addAll(mBaseSingletons);
+        return evalFuzzies(matchers, raw, h, state);
     }
 
+    private static void addContextual(List<FuzzyBaseMatcher> matchers,
+            CompState state)
+    {
+        for (CompCompanionBean comp : state.getUser().getCompanions())
+        {
+            final String name = state.resolve(comp.getName());
+            matchers.add(new FuzzyBaseMatcher(name, (s, h) -> h.doAbout(s, name)));
+        }
+    }
+    
+    private static List<FuzzyCombatMatcher> mCombatSingletons = new ArrayList<>();
+    private static void addCombatSingleton(String names, BiConsumer<CompState, CombatStateHandler> action)
+    {
+        for (StringTokenizer st = new StringTokenizer(names, ","); st.hasMoreTokens(); )
+            mCombatSingletons.add(new FuzzyCombatMatcher(st.nextToken(), action));
+    }
+    static
+    {
+        addCombatSingleton("runaway", (state,h) -> h.doMove(state));
+        addCombatSingleton("shoot,kill,fart,bite", (state,h) -> h.doFight(state));
+        addCombatSingleton("look,examine", (state,h) -> h.doLook(state));
+        addCombatSingleton("repeat,replay", (state,h) -> h.handleRepeat(state));
+        addCombatSingleton("help", (state,h) -> h.doHelp(state));
+        addCombatSingleton("turn off", (state,h) -> h.doStop(state));
+   }
+    
     public static void doDefault(CompState state, CombatStateHandler h)
     {
         if (doRawDefault(state, h))
@@ -259,46 +262,56 @@ public class DefaultLogic
         if (StringUtils.isTrivial(raw))
             return false;
         raw = raw.toLowerCase();
-        if (raw.indexOf("runaway") >= 0)
+        List<FuzzyCombatMatcher> matchers = new ArrayList<>();
+        matchers.addAll(mCombatSingletons);
+        return evalFuzzies(matchers, raw, h, state);
+    }
+}
+
+class FuzzyStateMatcher<T>
+{
+    public String text;
+    public BiConsumer<CompState, T> action;
+    public int count;
+    
+    public FuzzyStateMatcher(String text, BiConsumer<CompState, T> action)
+    {
+        this.text = text;
+        this.action = action;
+    }
+    
+    public boolean eval(String raw)
+    {
+        this.count = MathUtils.max(PhoneticMatchLogic.countWordMatches(raw, text));
+        return this.count == 100;
+    }
+    
+    public boolean exec(CompState state, T h)
+    {
+        try
         {
-            h.doMove(state);
+            action.accept(state, h);
             return true;
         }
-        if (raw.indexOf("shoot") >= 0)
-        {
-            h.doFight(state);
-            return true;
-        }
-        if (raw.indexOf("kill") >= 0)
-        {
-            h.doFight(state);
-            return true;
-        }
-        if (raw.indexOf("look") >= 0)
-        {
-            h.doLook(state);
-            return true;
-        }
-        if ((raw.indexOf("repeat") >= 0) || (raw.indexOf("replay") >= 0))
-        {
-            h.handleRepeat(state);
-            return true;
-        }
-        if (raw.indexOf("help") >= 0)
-        {
-            h.doHelp(state);
-            return true;
-        }
-        if (raw.indexOf("turn off") >= 0)
-        {
-            h.doStop(state);
-            return true;
-        }
-        if ((raw.indexOf("fart") >= 0) || (raw.indexOf("bite") >= 0))
-        {
-            h.doFight(state);
-            return true;
+        catch (IllegalStateException e)
+        {                
         }
         return false;
+    }
+}
+
+class FuzzyBaseMatcher extends FuzzyStateMatcher<BaseStateHandler>
+{
+    public FuzzyBaseMatcher(String text, BiConsumer<CompState, BaseStateHandler> action)
+    {
+        super(text, action);
+    }
+}
+
+class FuzzyCombatMatcher extends FuzzyStateMatcher<CombatStateHandler>
+{
+    public FuzzyCombatMatcher(String text, BiConsumer<CompState, CombatStateHandler> action)
+    {
+        super(text, action);
     }
 }
